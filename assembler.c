4efa692b26f7f604f68ea91201bc79eff4940605
welu3614 <welu3614@uni.sydney.edu.au>
2021-04-10 14:20:22 +1000

    changed

--- assembler.c
+++ assembler.c
@@ -2,43 +2,31 @@
 #include <stdlib.h>
 #include <string.h>
 #include <math.h>
-char universal_write_func(FILE * fw, char x, int bit_num, int * bit_ptr, char * atm) {
+void universal_write_func(FILE * fw, char x, int bit_num, int * bit_ptr, char * atm) {
     int multiple = *bit_ptr / 8;
     int remainder = *bit_ptr % 8;
-    //printf("bit_num: %d\n", bit_num);
-   //printf("atm: %d\n", *atm);
     if (remainder == 0) {
         char content = x << (8 - bit_num);
         fwrite(&content, 1, 1, fw);
         *atm = content;
     } else {
-        //printf("ptr: %d\n", *bit_ptr);
         if (bit_num + remainder > 8) {
-            //char atm = atm << (8 - remainder);
             char content = x >> ((bit_num + remainder) - 8);
             char result1 = *atm + content;
-            //printf("result1: %x\n", result1);
             fseek(fw, multiple, SEEK_SET);
             fwrite(&result1, 1, 1, fw);
             char result2 = x << (8 - ((bit_num + remainder) - 8));
-            //printf("result2: %x\n", result2);
             fwrite(&result2, 1, 1, fw);
             *atm = result2;
-            //printf("hello\n");
         } else {
             fseek(fw, multiple, SEEK_SET);
-            //printf("ho");
             char content  = x << (8 - bit_num - remainder);
             char result = *atm + content;
-            //printf("content: %x, result: %x, atm: %x\n", content, result, *atm);
-            //printf("result: %x\n", result);
             fwrite(&result, 1, 1, fw);
             *atm = result;
-            //printf("hi\n");
         }
     }
     *bit_ptr += bit_num;
-    //printf("ptr: %d\n", *bit_ptr);
 }
 
 char to_binary(char * num) {
@@ -70,6 +58,9 @@ char bi_from_code(char * tok) {
         return 0b110;
     } else if (!strcmp(tok, "EQU")) {
         return 0b111;
+    } else {
+        perror("unidentified instruction\n");
+        exit(1);
     }
 }
 
@@ -82,6 +73,9 @@ int get_type_len(char * tok) {
         return 5;
     } else if (!strcmp(tok, "PTR")) {
         return 5;
+    } else {
+        perror("unidentified type\n");
+        exit(1);
     }
 }
 char bi_from_type(char * tok) {
@@ -93,6 +87,9 @@ char bi_from_type(char * tok) {
         return 0b10;
     } else if (!strcmp(tok, "PTR")) {
         return 0b11;
+    } else {
+        perror("unidentified type\n");
+        exit(1);
     }
 }
 int main(int argc, char ** argv) {
@@ -105,31 +102,20 @@ int main(int argc, char ** argv) {
     char * tok1;
     char * tok2;
     char * tok3;
-    char * tok4;
     char * func = "FUNC";
-    char * mov = "MOV";
     char * cal = "CAL";
     char * ret = "RET";
-    char * add = "ADD";
     char * print = "PRINT";
-    char * ref = "REF";
     char * not = "NOT";
     char * equ = "EQU";
     char * val = "VAL";
     char * reg = "REG";
     char * stk = "STK";
     char * ptr = "PTR";
-    char data[4096];
-    int pt = 0;
-    //int inside_func = 1;
     while (fgets(line, 256, f)) {
         int len = strlen(line);
         line[len - 1] = '\0';
         tok1 = strtok(line, " ");
-        /*if (!inside_func) {
-            inside_func = 1;
-            continue;
-        }*/
         if (!strcmp(tok1, func)) {
             tok2 = strtok(NULL, " ");
             bit_count+= 3;
@@ -149,9 +135,6 @@ int main(int argc, char ** argv) {
                 bit_count += 5;
             }
             if (!strcmp(tok1, cal) || !strcmp(tok1, ret) || !strcmp(tok1, print) || !strcmp(tok1, not)|| !strcmp(tok1, equ)) {
-                if (!strcmp(tok1, ret)) {
-                    //inside_func = 0;
-                }
                 continue;
             }
             strtok(NULL, " ");
@@ -176,7 +159,7 @@ int main(int argc, char ** argv) {
     int total_bits = multiple * 8;
     int padding = total_bits - bit_count;
     fseek(f, 0, SEEK_SET);
-    FILE * fw = fopen("testcode", "wb");
+    FILE * fw = fopen(argv[2], "wb");
     
     int bit_ptr = 0;
     char pad = 0b0;
@@ -185,15 +168,12 @@ int main(int argc, char ** argv) {
     }
     char atm = 0;
     bit_ptr = padding;
-    //printf("bit_ptr: %d\n", bit_ptr);
     char * kot1;
     char * kot2;
     char * kot3;
     char * kot4;
     char * kot5;
-    int in_func = 1;
     int func_count = 0;
-    FILE * fr = fopen("testcode", "rb");
     while (fgets(line, 256, f)) {
         int len = strlen(line);
         line[len - 1] = '\0';
@@ -202,29 +182,18 @@ int main(int argc, char ** argv) {
             kot2 = strtok(NULL, " ");
             kot3 = strtok(NULL, " ");
             char content = atoi(kot3);
-            //printf("func: %d", content);
             universal_write_func(fw,content, 3, &bit_ptr, &atm);
             func_count = 0;
-            in_func = 1;
         } else if (!strcmp(kot1, ret)) {
             char content = 0b010;
             universal_write_func(fw,content, 3, &bit_ptr, &atm);
-            //printf("bit_ptr: %d !!!", bit_ptr);
             func_count ++;
             char count = func_count;
-            //printf("func count: %d!!!!!!!!!!!!!!!!!!!\n", func_count);
             universal_write_func(fw, count, 5, &bit_ptr, &atm);
         } else {
             kot2 = strtok(NULL, " ");
             kot3 = strtok(NULL, " ");
             if (!strcmp(kot1, cal) || !strcmp(kot1, ret) || !strcmp(kot1, print) || !strcmp(kot1, not)|| !strcmp(kot1, equ)) {
-                /*if (!strcmp(kot1, ret)) {
-                    func_count++;
-                    char count = func_count;
-                    printf("func count: %d!!!!!!!!!!!!!!!!!!!\n", func_count);
-                    universal_write_func(fw, count, 5, &bit_ptr, &atm);
-                    continue;
-                }*/
                 char content = to_binary(kot3);
                 universal_write_func(fw, content, get_type_len(kot2), &bit_ptr, &atm);
                 universal_write_func(fw, bi_from_type(kot2), 2, &bit_ptr, &atm);
@@ -236,7 +205,6 @@ int main(int argc, char ** argv) {
             kot5 = strtok(NULL, " ");
             char content2 = to_binary(kot3);
             char content1 = to_binary(kot5);
-            printf("kot5: %s, binary: %x\n", kot5, content1);
             universal_write_func(fw, content1, get_type_len(kot4), &bit_ptr, &atm);
             universal_write_func(fw, bi_from_type(kot4), 2, &bit_ptr,&atm);
             universal_write_func(fw, content2, get_type_len(kot2), &bit_ptr, &atm);
