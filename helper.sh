95954b35d457e8445ab8cebe553cc8a42ea0cb12
welu3614 <welu3614@uni.sydney.edu.au>
2021-04-12 23:47:10 +1000

    changed

--- helper.sh
+++ helper.sh
@@ -3,8 +3,17 @@ for testcase in `ls tests/*`;\
 do \
 if [ ${testcase: -4} == ".asm" ];\
 then\
-        filename=$(basename "$testcase" .asm);\
-        expectedfile=tests/${filename}_mil.out;\
-        cat $testcase > $expectedfile
+        #filename=$(basename "$testcase" .asm);\
+        #expectedfile=tests/${filename}_mil.out;\
+        #cat $testcase > $expectedfile
+        echo ${testcase:0:100}
 fi;\
 done;\
+
+#i=1
+#while [ $i != 20 ]
+#do 
+    #mv tests/${i} tests/test_invalid_random${i}.in
+    #echo $i
+    #(( i++ ))
+#done
--- test.sh
+++ test.sh
@@ -1,23 +1,36 @@
 #!/bin/bash
 
-for testcase in `ls tests/*`;\
-do \
-if [ ${testcase: -3} == ".in" ];\
-then\
-        filename=$(basename "$testcase" .in);\
-        expectedfile=tests/$filename.out;\
-        result=$(./vm_x2017 $testcase 2>&1| diff - $expectedfile);\
-        echo $result;\
-        if [ "$result" == "" ];\
-        then\
-        echo "vm $filename passed";\
-        fi;\
-        mil_expected=tests/${filename}_mil.out
-        result_mil=$(./objdump_x2017 $testcase | diff - $mil_expected)
-        echo $result_mil
-        if [ "$result_mil" == "" ];\
-        then\
-        echo "milstone $filename passed";\
-        fi;\
-fi;\
-done;\
+for testcase in `ls tests/*`
+do
+if [ ${testcase: -3} == ".in" ]
+then
+            filename=$(basename "$testcase" .in)
+            if [ ${filename:0:19} != "test_invalid_random" ]
+            then
+                expectedfile=tests/$filename.out
+                result=$(./vm_x2017 $testcase 2>&1| diff - $expectedfile)
+                #echo $result
+                if [ "$result" == "" ]
+                then
+                echo "vm $filename passed"
+                fi
+            else
+                ./vm_x2017 $testcase >/dev/null 2>&1
+                if [ $? == 1 ]
+                then
+                    echo "vm $filename passed"
+                fi
+
+            fi
+        if [ ${testcase:0:18} != "tests/test_invalid" ]
+        then
+            mil_expected=tests/${filename}_mil.out
+            result_mil=$(./objdump_x2017 $testcase | diff - $mil_expected)
+            #echo $result_mil
+            if [ "$result_mil" == "" ]
+            then
+            echo "milstone $filename passed"
+            fi
+        fi
+fi
+done
