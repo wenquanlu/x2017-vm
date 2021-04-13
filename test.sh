1363ba9ffd753c8312e33caddc4a4e66d4384a4e
welu3614 <welu3614@uni.sydney.edu.au>
2021-04-13 21:39:01 +1000

    changed

--- test.sh
+++ test.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-
+pass_vm=0
+fail_vm=0
+pass_mil=0
+fail_mil=0
 for testcase in `ls tests/*`
 do
 if [ ${testcase: -3} == ".in" ]
@@ -9,16 +12,24 @@ then
             then
                 expectedfile=tests/$filename.out
                 result=$(./vm_x2017 $testcase 2>&1| diff - $expectedfile)
-                echo $result
+                #echo $result
                 if [ "$result" == "" ]
                 then
-                echo "vm $filename passed"
+                    echo "vm $filename passed"
+                    (( pass_vm++ ))
+                else
+                    echo "vm $filename failed"
+                    (( fail_vm++ ))
                 fi
             else
                 ./vm_x2017 $testcase >/dev/null 2>&1
                 if [ $? == 1 ]
                 then
                     echo "vm $filename passed"
+                    (( pass_vm++ ))
+                else    
+                    echo "vm $filename failed"
+                    (( fail_vm++ ))
                 fi
 
             fi
@@ -26,11 +37,17 @@ then
         then
             mil_expected=tests/${filename}_mil.out
             result_mil=$(./objdump_x2017 $testcase | diff - $mil_expected)
-            echo $result_mil
+            #echo $result_mil
             if [ "$result_mil" == "" ]
             then
-            echo "milstone $filename passed"
+                echo "milstone $filename passed"
+                (( pass_mil++ ))
+            else
+                echo "milstone $filename failed"
+                (( fail_mil++ ))
             fi
         fi
 fi
 done
+echo passed $pass_vm vm testcases, fail $fail_vm.
+echo passed $pass_mil milstone testcases, fail $fail_mil.
